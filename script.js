0a7deb5ba68570164e97674ee37e474eb0689f9e
Jonshea Nutson <yuurieru@icloud.com>
2023-01-23 10:48:36 -0500

    pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+// game logic
+
+// make a board using a 2D array
+
+// be able to click board tiles
+
+// be able to add flags to tiles on click
+
+// populate board with mines
+
+// populate numbers in correspondence to mines
+
+// check for win/lose
+
+// reset button
