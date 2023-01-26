40b08091d8c28ff88d1e3cc72a9fb55a966c3af4
Jonshea Nutson <yuurieru@icloud.com>
2023-01-26 10:20:14 -0500

    added retry button

--- game2.js
+++ game2.js
@@ -1,10 +1,13 @@
 // variables
 const ejectBtn = document.querySelector('.material-symbols-sharp')
-
+const resetBtn = document.querySelector('.material-symbols-sharp')
 // functions
 const homePage = (e) => {
     location.replace('./home.html')
 }
 
+const retry = (e) => {
+    location.reload()
+}
 // event listeners
 ejectBtn.addEventListener('click', homePage)
