33873408e2c5833ed1f88ef28fdc9f047cf45e10
Jonshea Nutson <yuurieru@icloud.com>
2023-01-26 11:53:33 -0500

    surge fix

--- game2.js
+++ game2.js
@@ -3,7 +3,7 @@ const ejectBtn = document.querySelector('.material-symbols-sharp')
 const resetBtn = document.querySelector('.material-symbols-rounded')
 // functions
 const homePage = (e) => {
-    location.replace('./home.html')
+    location.replace(`https://mine_sweeper.surge.sh/`)
 }
 
 const retry = (e) => {
